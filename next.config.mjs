cc74ecb86c8b1049e41d0a2b5a7d218268e7af83
Nguyễn Ngọc Truyện <nntruyen027@gmail.com>
2026-01-18 08:21:53 +0700

    Test ci cd

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+  output: 'standalone',
 
-export default nextConfig;
+  reactCompiler: true,
+
+  // Khuyến nghị cho production
+  poweredByHeader: false,
+  compress: true,
+
+  // Nếu có gọi API backend khác domain
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://10.110.20.107:3001/api/:path*',
+  //     },
+  //   ]
+  // },
+}
+
+export default nextConfig
