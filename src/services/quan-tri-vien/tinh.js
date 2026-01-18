d590861fb9918fe119e92956c6822848ddc6a60a
Nguyễn Ngọc Truyện <nntruyen027@gmail.com>
2026-01-18 10:19:59 +0700

    bảo vệ base url tỉnh

--- src/services/quan-tri-vien/tinh.js
+++ src/services/quan-tri-vien/tinh.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BE;
+console.log(BASE_URL);
+
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BE + '/quan-tri/tinh',
+    baseURL: `${BASE_URL}/quan-tri/tinh`,
     headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${localStorage.getItem("jwtToken")}`,
